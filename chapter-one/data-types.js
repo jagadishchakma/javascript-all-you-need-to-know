49037469b92354fdcf2250f92ad02329a48239f9
jagadishchakma <jagadishchakma401@gmail.com>
2021-02-08 20:19:02 +0600

    data types discuss

---
+++ chapter-one/data-types.js
@@ -0,0 +1,21 @@
+/* primittive data types */
+// number 
+const roll = 23;
+// string 
+const name = 'Jagadish Chakma';
+// Boolean 
+const is = true/false;
+// umdefined
+const see = undefined;
+// null
+const empty = null;
+
+/*object or reference data types */
+// array
+const array = [];
+// function 
+function myName() {
+
+}
+// object
+const person = {}
---
+++ chapter-one/reserved-words.js
@@ -0,0 +1,4 @@
+// do not use reserve words as name
+
+const myName = 'Jagadish Chakma'
+console.log(myName)
