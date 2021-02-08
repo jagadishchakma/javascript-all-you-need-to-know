f2dbd6471bc40a6e5366bc93c6d198acd08ab227
jagadishchakma <jagadishchakma401@gmail.com>
2021-02-08 19:43:29 +0600

    javascript all you need to know

---
+++ chapter-one/variable.js
@@ -0,0 +1,8 @@
+const name = "Jagadish Chakma";
+const age = 23;
+const study = 'BBA 2nd year';
+const institute = 'Rangamati Govt College'
+
+console.log('My name is ' + name );
+console.log('I am '+age+'  years old');
+console.log('I study at '+ institute+ study)
