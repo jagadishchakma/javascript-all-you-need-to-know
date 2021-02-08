2d7131b38c364ee2d8a3d34f92c490b82bbf12ac
jagadishchakma <jagadishchakma401@gmail.com>
2021-02-08 20:55:37 +0600

    string and method

---
+++ chapter-one/string.js
@@ -0,0 +1,14 @@
+// single quote string
+const str1 = 'String one';
+
+// double quote string
+const str2 = "String two";
+
+// backtick string
+
+const str3 =   `Backtick string`;
+
+// conver a number to string 
+
+const n = 23;
+console.log(String(n))
