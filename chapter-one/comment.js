b07d21731498fb54228934ef70e3bacb102df575
jagadishchakma <jagadishchakma401@gmail.com>
2021-02-08 23:41:50 +0600

    js single and multi line comment

---
+++ chapter-one/comment.js
@@ -0,0 +1,10 @@
+// single line comment
+const name = 'Jagadish Chakma';
+/*
+name : 'jagadish Chakma,
+age : 23,
+study: BBA,
+institiute:'Rangamati Govt College'
+*/
+
+const got = 'Multi line comment';
--- chapter-one/date.js
+++ chapter-one/date.js
@@ -1,6 +1,8 @@
 const date = new Date();
 console.dir(date)
+
 console.log(date);
+console.log(typeof date)
 console.log(date.toDateString())
 console.log(date.toLocaleDateString());
 console.log(date.toTimeString());
@@ -14,4 +16,6 @@ console.log(date.getHours());
 console.log(date.getMinutes());
 console.log(date.getSeconds());
 console.log(date.getMilliseconds());
-console.log(date.getTime())
+console.log(date.getTime());
+console.dir(Math)
+console.log(typeof Math)
