b0b2e2cf94d6d5383c5f7aa362d9561d5f977446
jagadishchakma <jagadishchakma401@gmail.com>
2021-02-08 20:33:58 +0600

    number usable case and syntax on js

---
+++ chapter-one/number.js
@@ -0,0 +1,12 @@
+// integer number
+const n = 123;
+console.log(n);
+// floating number
+const floatN = 34.43;
+console.log(floatN);
+// convert string to float number
+const result = '23.4';
+console.log(parseFloat(result));
+// convert string to integer number
+const intN = '2323';
+console.log(parseInt(intN))
