6107ecba64bb54c3550d87af3f1e67b7113f3aa2
jagadishchakma <jagadishchakma401@gmail.com>
2021-02-09 08:05:40 +0600

    else if condition

---
+++ chapter-two/else-if.js
@@ -0,0 +1,8 @@
+const n = 12;
+if(n > 1){
+    console.log(`${n} is greater than 1`);
+} else if( n < 1) {
+    console.log(`${n } is less than 1`);
+} else {
+    console.log(`They Both Are Same`);
+}
---
+++ chapter-two/else.js
@@ -0,0 +1,6 @@
+const n = 5;
+if(n % 2 === 0){
+    console.log(`${n} is even number`);
+} else {
+    console.log(`${n} is odd number`);
+}
