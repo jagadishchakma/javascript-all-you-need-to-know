e604d197dc6d5d255e0a22b5008dbf42f0f3f0b9
jagadishchakma <jagadishchakma401@gmail.com>
2021-02-09 07:44:34 +0600

    dfd

---
+++ chapter-two/condition.js
@@ -0,0 +1,7 @@
+// in this chapter we are teaching conditional statements
+/*
+1. if condition
+2. else condition
+3. else if condition
+4. switch statements
+*/
---
+++ chapter-two/if.js
@@ -0,0 +1,20 @@
+const n1 = 23;
+const n2 = 243;
+
+if(n1 > n2){
+    console.log(`n1 is greather than n2`);
+}
+
+
+
+if(n1 < n2){
+    console.log(`n1 is less than n2`);
+}
+// find out who is odd and even number
+const n3 = 5;
+if(n3 % 2 === 0){
+    console.log(`n3 is even number`);
+}
+if(n3 % 2 === 1){
+    console.log(`n3 is odd number`);
+}
